c65c21a5eb465f9895c286ac52dced7e92160b79
Tasneem Nur <tasneemnurtashnia@gmail.com>
2023-06-08 23:50:42 +0600

    make a route for jwt and use a middleware for jwt.

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({error: true, message: 'unauthorized access'});
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9iyox0a.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,6 +42,18 @@ async function run() {
   try {
     await client.connect();
 
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn:'1h' })
+      res.send(token)
+    })
+
+
+    app.get('/users', verifyJWT, async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result)
+    })
     app.post('/users', async(req, res) => {
       const user = req.body;
       const query = {email: user.email}
@@ -31,10 +62,6 @@ async function run() {
         return 
       }
       const result = await userCollection.insertOne(user);
-      res.send(result)
-    })
-    app.get('/users', async(req, res) => {
-      const result = await userCollection.find().toArray();
       res.send(result)
     })
     app.patch('/users/instructor/:id', async(req, res) => {
