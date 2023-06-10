b9c628bdc84262f140a55be0375342860cc87af7
Tasneem Nur <tasneemnurtashnia@gmail.com>
2023-06-11 03:11:19 +0600

    payment intent api created.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -190,6 +191,18 @@ async function run() {
       res.send(result);
     })
     
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
